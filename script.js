e30160a49487e7b5c1cd3ccade2a2dc405ad512e
jhoto222 <tul26642@temple.edu>
2025-02-22 14:17:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+//alert('VIRUS!');
+//alert('JK i love you');
+function showImage() {  document.getElementById("sillyfaze").classList.remove("hidden");
+}
+function showImage2() {  document.getElementById("sillyfaze2").classList.remove("hidden");
+}
+function showImage3() {  document.getElementById("sillyfaze3").classList.remove("hidden");                     document.getElementById("silly4").classList.remove("hidden");
+}
+function text() {  
+  document.getElementById("silly6").classList.remove("hidden");
+  document.getElementById("silly5").classList.remove("hidden");}
